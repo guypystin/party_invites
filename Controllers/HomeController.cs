b5a727878cd7407bde2b239119e89ed84d44243f
guypystin <51515987+guypystin@users.noreply.github.com>
2021-07-15 23:51:21 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using partyInvites06.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace partyInvites06.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = 16;
+            ViewBag.GG = hour;
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RspvForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RspvForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace partyInvites06.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите своё имя")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите свою почту")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите номер телефона")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, укажите примете ли вы участие")]
+        public bool? WillAttend { get; set; }
+    }
+}
